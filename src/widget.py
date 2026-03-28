d826ff36dc08fc5148340050746d92e5814196bf
gefri <me@friks.ru>
2026-03-28 13:46:46 +0300

    added a function mask_account_card

---
+++ src/widget.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(data: Union[str]) -> Union[str]:
+    """Принимает строку, содержащую тип и номер карты или счета, и возвращает строку с замаскированным номером"""
+    parts = data.split()
+    number = parts[-1]
+    name = " ".join(parts[:-1])
+
+    if len(number) == 16:
+        masked = get_mask_card_number(number)
+    elif len(number) == 20:
+        masked = get_mask_account(number)
+    else:
+        raise ValueError("Неизвестный формат номера")
+
+    return f"{name} {masked}"
