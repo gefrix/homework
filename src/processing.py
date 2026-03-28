b57ba6a5e3c969bf90adbd0d7a33afd46a85fd97
gefri <me@friks.ru>
2026-03-29 00:27:27 +0300

    feat: implement sorting logic

--- src/processing.py
+++ src/processing.py
@@ -14,3 +14,12 @@ def filter_by_state(operations_list: List[Dict[str, Any]], state: str = "EXECUTE
                 result.append(operation)
 
     return result
+
+
+def sort_by_date(operations_list: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
+    """
+    Функция принимает список словарей и необязательный параметр, задающий порядок сортировки.
+    Возвращает новый отсортированный по дате список
+    """
+    sorted_operations_list = sorted(operations_list, key=lambda operation: operation["date"], reverse=reverse)
+    return sorted_operations_list
